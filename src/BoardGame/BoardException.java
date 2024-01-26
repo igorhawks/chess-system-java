d09b91d1d5cec7e66d48d38d016b9875b1cb7e7e
Igor Silva <igor719@gmail.com>
2024-01-26 16:51:28 -0300

    BoardException

---
+++ src/BoardGame/BoardException.java
@@ -0,0 +1,11 @@
+package BoardGame;
+
+public class BoardException extends RuntimeException{
+    public static  final long serialVersionUID = 1L;
+
+    public BoardException(String msg)
+    {
+        super((msg));
+    }
+
+}
