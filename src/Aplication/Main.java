a07aed6583e92277d6d0dcc314795f47b1984996
Igor Silva <igor719@gmail.com>
2024-01-29 14:51:13 -0300

    CheckMate

--- src/Aplication/Main.java
+++ src/Aplication/Main.java
@@ -18,7 +18,7 @@ public class Main {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true)
+        while (!chessMatch.getCheckMate())
         {
             try {
                 UI.clearScreen();
@@ -51,6 +51,7 @@ public class Main {
             }
         }
 
-
+        UI.clearScreen();
+        UI.printMatch(chessMatch,captured);
     }
 }
